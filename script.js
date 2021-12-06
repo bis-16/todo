d865942256d50121bd05522e41a9964d3a2fa104
bis-16 <s1ash@live.ru>
2021-12-06 14:44:07 +0700

    редизайн, рефакторинг, починил галочку избранного

--- script.js
+++ script.js
@@ -2,109 +2,122 @@
 document.addEventListener('DOMContentLoaded',()=> {
 
 
-let parentToDoForm = document.querySelector('.tasks'),
-            btnAdd = document.querySelector("#btn__add"),
-             input = document.querySelector("#input1");
 
-//console.log(parentToDoForm)
-//console.log(1);
+    let         btnAdd = document.querySelector("#btn__add"),
+                 input = document.querySelector("#input1"),
+             tasksForm = document.querySelector(".tasks")
 
-let tasks = []
+    //console.log(parentToDoForm)
+    //console.log(1);
 
-class Task {
-    constructor (value) {
-        this.value = input.value
-        this.checked = false
-        this.favorite = false
-    }
-    render(value) {
-        parentToDoForm.innerHTML += `
-        <div class="div__checkbox">
-            <div class="div__checkbox__1">
-                <input type="checkbox" class="checkbox" >
-                ${this.value}
+    let tasks = []
+
+    class Task {
+        constructor (value) {
+            this.value = input.value
+            this.checked = false
+            this.favorite = false
+        }
+        render() {
+            let checked = "", favChecked = "", favorite = ""
+
+            if (this.checked === true) checked = "checked"
+            if (this.favorite === true) {
+                favChecked = "checked"
+                favorite = "favorite"
+            }
+
+            console.log("this=",this)
+
+            tasksForm.innerHTML += `
+            <div class="div__checkbox ${checked} ${favorite}">
+                <div class="div__checkbox__1">
+                    <input type="checkbox" class="checkbox" ${checked}>
+                    ${this.value}
+                </div>
+                <div class="div__checkbox__2">
+                    <input type="checkbox" class="favoriteClass" ${favChecked}>
+                    <button class="btn__remove">X</button>
+                </div>
             </div>
-            <div class="div__checkbox__2">
-                <input type="checkbox" class="favorite">
-                <button class="btn__remove">X</button>
-            </div>
-        </div>
+    
+           `
+        }
+        remover() {
 
-       `
+        }
     }
-    remover(){
 
-    }
-}
-
-function drawAllTasks(taskValue=0) {
-    console.log('tasks=',tasks)
-    parentToDoForm.innerHTML = ""
-    tasks.forEach(item => item.render() )
-}
-
-function checkedTask() {
-    let btnChk = document.querySelectorAll(".checkbox")
-    //console.log('chk1');
-    btnChk.forEach((item, id) => {
-        //console.log('chk2');
-        item.addEventListener('click',(event)=>{
-            //console.log('chk3');
-            //event.preventDefault();
-            //let target = event.target
-            //console.log('item=',item);
-            console.log('item.parentNode=',item.parentNode.parentNode)
-            item.parentNode.parentNode.classList.toggle("checked")
+    function drawAllTasks(taskValue= 0) {
+        console.log('tasks=',tasks)
+        tasksForm.innerHTML = ""
+        tasks.forEach((item, id) => {
+            item.render()//.bind(item)
         })
-    })
 
-}
-
-function removeTask() {
-    //console.log('remove1');
-    let btnRemove = document.querySelectorAll(".btn__remove")
-    btnRemove.forEach((item,id) => {
-        //console.log('remove2');
-        //console.log('item2=',item);
-        item.addEventListener('click',(event) => {
-            event.preventDefault();
-            //console.log('event3=',event)
-            //console.log('remove3');
-            //console.log('parentToDoForm=',parentToDoForm);
-            //console.log('event=',event);
-            //console.log('item=',item);
-            //console.log('item.parentNode=',item.parentNode.parentNode.remove());
-            item.parentNode.parentNode.remove()
-            //console.log('id',id);
-            tasks.splice(id,1);
-            //item.remove();
-         });
-
-    })
-}
+        checkedTask()
+        removeTask();
+    }
 
 
+    // let checkVerification = (selector) => {
+    //
+    // }
 
-btnAdd.addEventListener('click',(event) => {
-    event.preventDefault();
-    console.log('parentToDoForm=',parentToDoForm);
-    let newTask = new Task(input.value)
-    tasks.push(newTask)
+    function checkedTask() {
+        let btnChk = document.querySelectorAll(".checkbox")
+        let btnFav = document.querySelectorAll(".favoriteClass")
+        btnChk.forEach((item, id) => {
+            item.addEventListener('click',(event)=>{
+                item.parentNode.parentNode.classList.toggle("checked")
+                tasks[id].checked = !tasks[id].checked;
+            })
+        })
+        btnFav.forEach((item, id) => {
+            item.addEventListener('click',(event)=>{
+                item.parentNode.parentNode.classList.toggle("favorite")
+                tasks[id].favorite = !tasks[id].favorite;
+            })
+        })
+    }
 
-
-    //newTask.render(input.value)
-    drawAllTasks()
-    checkedTask()
-    removeTask();
-});
+    function removeTask() {
+        let btnRemove = document.querySelectorAll(".btn__remove")
+        btnRemove.forEach((item,id) => {
+            item.addEventListener('click',(event) => {
+                event.preventDefault();
+                tasks.splice(id,1);
+                drawAllTasks()
+             });
+        })
+    }
 
 
 
+    btnAdd.addEventListener('click',(event) => {
+        event.preventDefault();
+        let buttonForm = document.querySelector(".buttons")
+        let errElem = document.createElement('div').textContent = "error"
 
+        if (input.value !== "") {
+            let newTask = new Task(input.value)
+            tasks.push(newTask)
+            drawAllTasks()
+        }
+        // else {
+        //
+        //     console.log(buttonForm);
+        //     console.log(errElem);
+        //     buttonForm.after(errElem)
+        //     //buttonForm.appendChild("фыва")
+        //     console.error("er")
+        //
+        // }
 
-
-
-
-
+        //
+        // checkedTask()
+        // checkedFav()
+        // removeTask();
+    });
 
 })
